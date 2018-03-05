0eff2a86176730329ce4796f11a8df72e0d9b374
Francesca Giordano <fgiordano.data@gmail.com>
2018-03-05 09:57:22 +0000

    Fixed output and folder links in yeast example

--- example/run_yeast.sh
+++ example/run_yeast.sh
@@ -19,14 +19,18 @@ rm -rf $output
 mkdir -p $output 
 cd $output
 
-echo Looking for Presence PAVs &> log.txt
-$scanPAV_version/scanPAV -nodes $cpus -score $score -align $aligner $assembly1 $assembly2  pavs_present_in_$outname &>> log.txt
-echo; echo Looking for Absence PAVs &>> log.txt
-$scanPAV_version/scanPAV -nodes $cpus -score $score -align $aligner $assembly2 $assembly1  pavs_absent_in_$outname  &>> log.txt
+ln -sf $assembly1 .
+ln -sf $assembly2 .
 
-echo; echo "Pipeline output can be found in " bwa_550/log.txt
+echo; echo Looking for Presence PAVs | tee log.txt ; echo
+$scanPAV_version/scanPAV -nodes $cpus -score $score -align $aligner $(basename $assembly1) $(basename $assembly2)  pavs_present_in_$outname | tee -a  log.txt
+echo; echo Looking for Absence PAVs | tee -a log.txt ; echo
+$scanPAV_version/scanPAV -nodes $cpus -score $score -align $aligner $(basename $assembly2) $(basename $assembly1)  pavs_absent_in_$outname  | tee -a  log.txt
+
+echo; echo; echo "Pipeline output can be found in " bwa_550/log.txt
 echo;echo Presence/Absence PAVs are in the $output folder:
 echo " " $output/pavs_present_in_$outname.fasta : pavs present in $(basename $assembly1) but absent in $(basename $assembly2)
 echo "   Details of presence PAVs are listed in the vcf file:" $output/pavs_present_in_$outname 
 echo;echo " " $output/pavs_absent_in_$outname.fasta : pavs absent in $(basename $assembly1) but present in $(basename $assembly2)
 echo "   Details of absence PAVs are listed in the vcf file:" $output/pavs_absent_in_$outname 
+echo
